874b95c326bb089f5cb26d418e012b7fa79820b0
nagesha technology <asbconstruction19@gmail.com>
2024-09-12 17:35:59 +0530

    started grafana server

--- variables.tf
+++ variables.tf
@@ -4,11 +4,11 @@ variable "tools" {
       instance_type        = "t3.small"
       policy_resource_list = ["ec2:DescribeInstances"]
     }
-#
-#     grafana = {
-#       instance_type        = "t3.small"
-#       policy_resource_list = []
-#     }
+
+    grafana = {
+      instance_type        = "t3.small"
+      policy_resource_list = []
+    }
 
     vault = {
       instance_type        = "t3.small"
